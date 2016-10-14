ed3ce56eb2dc7fbac23a4296742788f7f1910493
Florent Messa <florent.messa@gmail.com>
2016-10-14 16:49:47 +0200

    Add missing setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+import os
+from setuptools import setup, find_packages
+
+root = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(root, 'README.rst')) as f:
+    README = f.read()
+
+setup(
+    name='django-signals-wtf',
+    version='0.0.0',
+    description='Deal with signals',
+    long_description=README,
+    author='Florent Messa',
+    author_email='florent.messa@gmail.com',
+    url='http://github.com/thoas/django-signals-wtf',
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Utilities',
+    ]
+)
