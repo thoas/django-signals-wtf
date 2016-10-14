f81a47f0cf64adba2646ab93bdab1b7d3c4913e6
Florent Messa <florent.messa@gmail.com>
2016-10-14 16:54:45 +0200

    :poop:

--- setup.py
+++ setup.py
@@ -1,17 +1,12 @@
 # -*- coding: utf-8 -*-
-import os
 from setuptools import setup, find_packages
 
-root = os.path.abspath(os.path.dirname(__file__))
-
-with open(os.path.join(root, 'README.md')) as f:
-    README = f.read()
 
 setup(
     name='django-signals-wtf',
     version='0.0.0',
     description='Deal with signals',
-    long_description=README,
+    long_description='Same as description',
     author='Florent Messa',
     author_email='florent.messa@gmail.com',
     url='http://github.com/thoas/django-signals-wtf',
